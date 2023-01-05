0961b1d623c6f79e5d1c604f8aefc79c4b21c041
Mariusz Morawski <marmor19@gmail.com>
2023-01-05 01:09:11 +0100

    Add movement and resizing

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,20 +1,71 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
-export type CardProps = {
+export type Cords = {
+  x: number;
+  y: number;
+};
+
+export enum MovementMode {
+  Moving = 1,
+  Resizing = 2,
+}
+
+export type CardType = {
   id: string;
   owner: string;
   text: string;
-  position: {
-    x: number;
-    y: number;
-  };
-  size: {
-    x: number;
-    y: number;
-  };
+  position: Cords;
+  size: Cords;
 };
 
-const Card: React.FC<CardProps> = ({ id, owner, text, position, size }) => {
+export type CardProps = {
+  card: CardType;
+  onSelectEnd: () => void;
+  onCardSelect: (id: string, offset: Cords, mode: MovementMode) => void;
+};
+
+export type EventParams = Pick<
+  React.MouseEvent<HTMLDivElement, MouseEvent>,
+  "clientX" | "clientY" | "target"
+>;
+
+const Card: React.FC<CardProps> = ({ card, onCardSelect, onSelectEnd }) => {
+  const { id, owner, position, size, text } = card;
+  const parentRef = useRef<HTMLDivElement | null>(null);
+
+  const onMouseDown =
+    (mode: MovementMode) =>
+    (
+      e: React.MouseEvent<HTMLDialogElement | HTMLButtonElement, MouseEvent> &
+        React.TouchEvent<HTMLDialogElement | HTMLButtonElement>
+    ) => {
+      e.stopPropagation();
+      const cords = e.touches ? e.touches[0] : e;
+      if (mode === MovementMode.Moving) {
+        onCardSelect(
+          card.id,
+          {
+            x: cords.clientX - e.currentTarget.offsetLeft,
+            y: cords.clientY - e.currentTarget.offsetTop,
+          },
+          mode
+        );
+      } else {
+        const buttonPosition = e.currentTarget.getBoundingClientRect();
+        const offsetInButton = {
+          x: buttonPosition.width - (e.clientX - buttonPosition.left),
+          y: buttonPosition.height - (e.clientY - buttonPosition.top),
+        };
+        const bounding = parentRef.current?.getBoundingClientRect();
+        if (!bounding) return;
+        const actionOffset = {
+          x: bounding.left - offsetInButton.x,
+          y: bounding.top - offsetInButton.y,
+        };
+        onCardSelect(card.id, actionOffset, mode);
+      }
+    };
+
   return (
     <div
       className="absolute border border-black rounded-md bg-white flex flex-col"
@@ -24,6 +75,11 @@ const Card: React.FC<CardProps> = ({ id, owner, text, position, size }) => {
         left: position.x,
         top: position.y,
       }}
+      ref={parentRef}
+      onMouseDown={onMouseDown(MovementMode.Moving)}
+      onMouseUp={onSelectEnd}
+      onTouchStart={onMouseDown(MovementMode.Moving)}
+      onTouchEnd={onSelectEnd}
     >
       <div className="w-full overflow-hidden flex flex-nowrap">
         <div className="text-red-500 text-base whitespace-nowrap flex-1 text-ellipsis flex-shrink-0 overflow-hidden">
@@ -31,7 +87,16 @@ const Card: React.FC<CardProps> = ({ id, owner, text, position, size }) => {
         </div>
         <button className="material-icons ">close</button>
       </div>
-      <div className="w-full">{text}</div>
+      <div className="w-full flex-grow">{text}</div>
+      <div className="w-full flex justify-end">
+        <button
+          className="material-icons rotate-90"
+          onMouseDown={onMouseDown(MovementMode.Resizing)}
+          onTouchStart={onMouseDown(MovementMode.Resizing)}
+        >
+          open_in_full
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,3 +1,11 @@
+import Card, {
+  CardProps,
+  CardType,
+  Cords,
+  EventParams,
+  MovementMode,
+} from "../components/Card";
+import React, { useEffect, useState } from "react";
 import { collection, doc, getFirestore, query } from "firebase/firestore";
 import {
   useCollection,
@@ -5,8 +13,6 @@ import {
   useDocumentData,
 } from "react-firebase-hooks/firestore";
 
-import Card from "../components/Card";
-import React from "react";
 import collectionToData from "../utils/collectionToData";
 import firestoreApp from "../firestoreApp";
 import { getAuth } from "firebase/auth";
@@ -15,19 +21,72 @@ import { useParams } from "react-router";
 
 const auth = getAuth(firestoreApp);
 
+type MovementDetails = {
+  id: string;
+  offset: Cords;
+  mode: MovementMode;
+};
+
 const Board: React.FC = () => {
   const [user] = useAuthState(auth);
   const { id } = useParams();
   const [cardsCol] = useCollection(
     query(collection(getFirestore(firestoreApp), `boards/${id}/cards`))
   );
+  const [cards, setCards] = useState<CardType[]>([]);
 
-  const [borad, loading, error] = useDocumentData();
-  const cards = collectionToData(cardsCol);
+  useEffect(() => {
+    setCards(collectionToData(cardsCol));
+  }, [cardsCol]);
+
+  const [movementDetails, setMovementDetails] = useState<MovementDetails>();
+
+  const onMouseMove = (e: EventParams) => {
+    if (!movementDetails) return;
+    const { offset, id, mode } = movementDetails;
+    const currentCard = cards.find((card) => card.id === id);
+    if (!currentCard) return;
+    const cardChange: Partial<CardType> = {};
+
+    if (mode === MovementMode.Moving) {
+      const x = e.clientX - offset.x;
+      const y = e.clientY - offset.y;
+      cardChange.position = { x, y };
+    } else {
+      const x = e.clientX - offset.x;
+      const y = e.clientY - offset.y;
+      cardChange.size = { x, y };
+    }
+
+    setCards((cards) =>
+      cards.map((card) => {
+        if (card.id === id) {
+          return { ...card, ...cardChange };
+        }
+        return card;
+      })
+    );
+  };
+
+  const onCardSelect = (id: string, offset: Cords, mode: MovementMode) => {
+    setMovementDetails({ id, offset, mode });
+  };
+
   return (
-    <div className="w-full h-full relative">
-      {cards.map((card) => (
-        <Card key={card.id} {...card} />
+    <div
+      className="w-full h-full relative"
+      onMouseMove={onMouseMove}
+      onTouchMove={(e) => {
+        onMouseMove(e.touches[0]);
+      }}
+    >
+      {Array.from(cards.values()).map((card) => (
+        <Card
+          card={card}
+          key={card.id}
+          onCardSelect={onCardSelect}
+          onSelectEnd={() => setMovementDetails(undefined)}
+        />
       ))}
     </div>
   );
